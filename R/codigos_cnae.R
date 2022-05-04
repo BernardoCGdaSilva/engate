03803c3c8135a1ad5bb21028f2ead14aba5587c5
Bernardo Cainelli Gomes da Silva <bernardo.gomes@fiergs.org.br>
2022-05-04 11:05:14 -0300

    nomes_cbo aprimorada para double

--- R/codigos_cnae.R
+++ R/codigos_cnae.R
@@ -1,7 +1,7 @@
 #' Códigos Classificação Nacional de Atividades Econômicas
 #' @encoding UTF-8
 #' @param tabela Data.frame: a tabela para adicionar outros níveis CNAE
-#' @param subclasse Caracter: a coluna com os códigos subclasse
+#' @param campo Caracter: a coluna com os códigos subclasse
 #' @param nivel Caracter: o nível CNAE desejado. Deve ser "Seção", "Divisão", "Grupo" ou "Classe"
 #' @param add_nomes Logical: deseja adicionar uma coluna com os nomes?
 #'
@@ -9,6 +9,15 @@
 #' @export
 #'
 #' @examples
-codigos_CNAE <- function(tabela,subclasse,nivel,add_nomes=F) {
-
+codigos_CNAE <- function(tabela,campo,nivel,add_nomes=F) {
+#x <- dplyr::mutate(tabela, cod = sprintf("%07d",tabela$campo))
+#return(x)
 }
+
+df1 <- data.frame(col1 = c(9609201, 111302))
+df2 <- data.frame(col1 = c("9609201", "111302"))
+codigos_CNAE(df1, "col1")
+
+
+a <- dplyr::mutate(df1, cod = sprintf("%07d",df1$col1))
+b <- dplyr::mutate(df2, cod = sprintf("%07s",df2$col1))
--- R/nomes_cbo.R
+++ R/nomes_cbo.R
@@ -8,9 +8,21 @@
 #'
 #' @examples
 #' df1 <- data.frame(col1 = c("010105", "515220", "992115"))
-#' df2 <- nomes_cbo(df1,"col1")
+#' df2 <- nomes_cbo(df1, "col1")
 #' df2
-nomes_cbo <- function(tabela,campo){
-  x <- merge(tabela, suporte_cbo, by.x = campo, by.y = "cod", all.x = T)
+#' df3 <- data.frame(col1 = c(010105, 515220, 992115))
+#' df4 <- nomes_cbo(df3, "col1")
+#' df4
+nomes_cbo <- function(tabela, campo) {
+  cod_caracter <- NULL
+  teste <- subset(tabela, select = campo) %>% unlist()
+
+  if (is.numeric(teste)) {
+    x <- dplyr::mutate(tabela, cod_caracter = sprintf(sprintf("%06d", teste))) %>%
+      merge(suporte_cbo, by.x = "cod_caracter", by.y = "cod", all.x = T) %>%
+      subset(select = -cod_caracter)
+  } else {
+    x <- merge(tabela, suporte_cbo, by.x = campo, by.y = "cod", all.x = T)
+  }
   return(x)
 }

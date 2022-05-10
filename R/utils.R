891370c74bb30e96a892eaa236dcc6fb05340f86
Bernardo Cainelli Gomes da Silva <bernardo.gomes@fiergs.org.br>
2022-05-10 16:00:03 -0300

    WIP

---
+++ R/utils.R
@@ -0,0 +1,16 @@
+### Set-up
+
+library(devtools)
+library(testthat)
+check()
+
+#_____________________________________________________________________________________________________________________
+### Construção de Site
+
+# Run once to configure package to use pkgdown
+usethis::use_pkgdown()
+# Run to build the website
+pkgdown::build_site()
+
+# If you’re using GitHub, we also recommend setting up GitHub actions to automatically build and publish your site:
+usethis::use_pkgdown_github_pages()
